a81f0b9c5d98d99f035445f191515451d0e3ec40
Rafael Bretas <rafael.bretas2000@gmail.com>
2016-07-01 11:10:12 -0300

    Trabalho dos Primos
    Guilherme Santana Anhetti, Mateus da Silva Mariano e Rafael Bretas

---
+++ numerosprimos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosprimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calcprimos y = new calcprimos();
+
+            Console.WriteLine("Ate que n°?");
+            y.limit = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+
+            y.initNumbers(y.limit);
+             
+            Console.ReadKey();
+
+            y.achareeliminar();
+            y.espera();
+            y.numerosparastring();
+        }
+    }
+}
---
+++ numerosprimos/calcprimos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosprimos
+{
+    class calcprimos
+    {
+        public int limit = 30;
+        public List<int> numeros = new List<int>();
+        public int currentPosition = 1;
+         
+
+        public void initNumbers(int limit) {
+            for (int i = 2; i <= limit; i++) {
+                
+                Console.WriteLine(i.ToString());
+                numeros.Add(i);
+            }
+        }
+
+        public void achareeliminar() {
+            currentPosition++;
+            if (numeros.Count > currentPosition) { 
+            int currentValue = numeros.ElementAt(currentPosition);
+
+                for (int i = currentPosition; i < numeros.Count; i++)
+                {
+                    Console.WriteLine(numeros.ElementAt(i));
+                    if (numeros.ElementAt(i) % currentValue == 0) 
+                    {
+                        numeros.RemoveAt(i);
+                        numeros.Remove(10);
+                    }
+
+                }
+                Console.ReadKey();
+                espera();
+                achareeliminar();
+            }
+        }
+        public void numerosparastring() {
+            foreach (var item in numeros) { 
+                Console.Write(item.ToString() + " ,");
+            }
+            espera();
+        }
+        public void espera() {
+            Console.ReadKey();
+        }
+    }
+}
